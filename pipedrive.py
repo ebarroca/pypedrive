b46274f85074fb6126f2d17f1c878ce3cfc28c16
ebarroca <ebarroca@nuxeo.com>
2015-08-09 21:35:58 -0400

    add write support for objects

--- pipedrive.py
+++ pipedrive.py
@@ -1,5 +1,6 @@
 import requests
-from util import *
+from .util import *
+import json
 
 API_ENDPOINT = "https://api.pipedrive.com/v1"
 
@@ -21,7 +22,7 @@ class PipedriveClient():
         (r, resource) = self._fetch_resource_by_id(res, id)
         return (r, resource)
 
-    def fetch_resource_by_id(self, resource, id):
+    def _fetch_resource_by_id(self, resource, id):
 
         if id is None:
             raise Exception("Can't fetch, ID given is None.")
@@ -32,11 +33,8 @@ class PipedriveClient():
         pass
 
     def _fetch_resource(self, resource, rid=None):
-        res = resource
-        url = self.endpoint + "/" + res
-        if rid is not None:
-            url = url + "/" + str(rid)
-        url = url + "?api_token=%s" % (self.api_token)
+        url = self._build_url(resource, rid)
+        debug("Fetching resource %s (%s) at %s" % (rid, resource, url))
         r = requests.get(url)
         data = r.json()
 
@@ -53,6 +51,8 @@ class PipedriveClient():
         return (r, data["data"])
 
     def load_fields_for_resource(self, resource):
+
+        debug("Loading fields for resource %s" % resource)
         fields = {}
         key_to_attr = {}
         attr_to_key = {}
@@ -69,13 +69,22 @@ class PipedriveClient():
         fields["attr_to_key"] = attr_to_key
         self.fields[resource] = fields
 
-    def update_score(self, person_id, score):
-        res = "/persons"
-        url = self.endpoint + res + \
-            "/%s?api_token=%s" % (person_id, self.api_token)
-        data = {LeadScoreID: score}
-        r = requests.put(url, data)
-        log(r.status_code)
+    def update_resource(self, resource, rid, data):
+        url = self._build_url(resource, rid)
+        # XXX todo Validate resource/id/data types
+        debug("Updating resource %s (%s) at %s" % (rid, resource, url))
+        headers = {"Content-Type": "application/json"}
+        r = requests.put(url, data=json.dumps(data), headers=headers)
+        debug("Update status code: %s" % r.status_code)
+        r.raise_for_status()
+        return
+
+    def _build_url(self, resource, rid=None):
+        url = self.endpoint + "/" + resource
+        if rid is not None:
+            url = url + "/" + str(rid)
+        url = url + "?api_token=%s" % (self.api_token)
+        return url
 
 
 class BaseResource(object):
@@ -88,19 +97,18 @@ class BaseResource(object):
         self._client = client
         self._resource = self.RESOURCE
         self.id = id
-        self._data_cache = None
+        self._data_cache = {}
         self.url = None
         self._fields = None
+        self._dirty_fields = set()
 
         if self._resource not in self._client.fields:
             self._client.load_fields_for_resource(self._resource)
+        self._init_done = True
 
     def __getattr__(self, name):
 
-        if self.HAS_CUSTOM_FIELDS and name in self._field_names:
-            attr = self._field_names[name]
-        else:
-            attr = name
+        attr = self._name_to_attr(name)
 
         if attr in self._data:
             value = self._data[attr]
@@ -108,10 +116,48 @@ class BaseResource(object):
         else:
             raise AttributeError
 
+    def __setattr__(self, name, value):
+        #print("Setting %s to %s" % (name, value))
+        if "_init_done" not in self.__dict__ or name in self.__dict__:
+            # use default setattr
+            object.__setattr__(self, name, value)
+        elif name in self.field_names:
+            # custom field, let's set it
+            attr = self._name_to_attr(name)
+            self._data_cache[attr] = value
+            self._dirty_fields.add(name)
+        else:
+            raise AttributeError("Can't set propery: attribute %s \
+                                          not found." % (name))
+
+    def _name_to_attr(self, name):
+        "Convert property name in pipedrive's object internal key."
+        if self.HAS_CUSTOM_FIELDS and name in self.field_names:
+            attr = self.field_names[name]
+        else:
+            attr = name
+        return attr
+        # Check if test on HAS_CUSTOM_FIELDS is required
+
+    def save(self):
+        "Save data back to pipedrive"
+        if not self._dirty_fields:
+            debug("No dirty fields for object %s" % self.id)
+            return
+        debug("Saving dirty fields: %s" % self._dirty_fields)
+        data_for_update = {}
+        for f in self._dirty_fields:
+            attr = self._name_to_attr(f)
+            data_for_update[attr] = self._data[attr]
+        self._client.update_resource(self.RESOURCE_SEGMENT, self.id,
+                                     data_for_update)
+        self._dirty_fields.clear()
+        self._data.clear()
+
     @property
     def _data(self):
-        if self._data_cache is None:
-            (r, data) = self._client.fetch_resource_by_id(
+        if not self._data_cache:
+            (r, data) = self._client._fetch_resource_by_id(
                 self.RESOURCE_SEGMENT, self.id)
             self.url = r.url
             self._data_cache = data
@@ -119,15 +165,15 @@ class BaseResource(object):
         return self._data_cache
 
     @property
-    def _field_keys(self):
+    def field_keys(self):
         if not self.HAS_CUSTOM_FIELDS:
-            return None
+            return []
         return self._client.fields[self._resource]["key_to_attr"]
 
     @property
-    def _field_names(self):
+    def field_names(self):
         if not self.HAS_CUSTOM_FIELDS:
-            return None
+            return []
         return self._client.fields[self._resource]["attr_to_key"]
 
 
